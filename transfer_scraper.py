e8e9d8311264890bee60d814442431cdf75e7f67
Wadir Malik <wadir.malik@xandertalent.com>
2023-03-21 17:34:47 +0000

    fixed bugs and scraped whole table

--- transfer_scraper.py
+++ transfer_scraper.py
@@ -3,49 +3,21 @@ import requests
 
 class TransferScraper():
 
-    def __init__(self, URL: str = 'https://www.transfermarkt.co.uk/premier-league/marktwerte/wettbewerb/GB1'):
-        headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"}
-        r = requests.get(url=URL, headers=headers)
-        self.soup = BeautifulSoup(r.content, 'html.parser')
+    def __init__(self):
+        self.headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"}
 
     def find_player_detail(self):
-        players_list = []
-        age_list = []
-        value_list = []
-        nationality_list = []
+        for pagenum in range(1, 5):
+            URL = "https://www.transfermarkt.co.uk/premier-league/marktwerte/wettbewerb/GB1/page/" + str(pagenum)
+            r = requests.get(url=URL, headers=self.headers)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            table = soup.find("table", class_='items')
+            tbody = table.find("tbody")
+            rows = tbody.find_all("tr")
 
-        players = self.soup.find_all("td", class_="hauptlink")
-        ages = self.soup.find_all("td", class_="zentriert")
-        nationality = self.soup.find_all("td", class_="zentriert")
-        values = self.soup.find_all("td", class_="rechts hauptlink")
+            for row in rows:
+                cols = row.find_all('td')
+                cols = [col.text.strip() for col in cols]
+                print(cols)
 
-        for player in players:
-            players_list.append(player.text)
-
-        i = 1
-        while i < len(players_list):
-            del players_list[i]
-            i += 1
-
-        for age in ages:
-            age_list.append(age.text)
-
-        new_age_list = []
-        for i in range(2, len(age_list), 4):
-            new_age_list.append(age_list[i])
-
-        for nation in nationality:
-            flags = nation.find_all('img')  
-            if flags:  
-                first_flag = flags[0]  
-                first_nationality = first_flag['title']
-                nationality_list.append(first_nationality)
-
-        i = 1
-        while i < len(nationality_list):
-            del nationality_list[i]
-            i += 1   
-
-        for value in values:
-            value_list.append(value.text)    
         
