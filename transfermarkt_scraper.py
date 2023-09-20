6050c4e7c3b24f201241f6069a8733602d853ce6
wadirmalik1 <wadirmalik@hotmail.co.uk>
2023-09-20 14:08:18 +0100

    added all players from all leagues

--- transfermarkt_scraper.py
+++ transfermarkt_scraper.py
@@ -6,8 +6,14 @@ class TransfermarktScraper:
 
     USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
     url_temp = "https://www.transfermarkt.co.uk/jumplist/startseite/wettbewerb/{}"
-    league_codes = ["GB1", "FR1", "L1", "IT1", "ES1"]
-    list_seasons = ["2016", "2017", "2018", "2019", "2020", "2021", "2022"]
+    league_codes = ["AN1L", "AR1N", "AUS1", "A1", "BGD1", "BE1", "BO1A", "BRA1", "CDN1", "CLPD", "CSL", "COLP", "COL1", "CRPD", "PDV1", 
+                    "KR1", "TS1", "DK1", "EL1A", "EL1S", "SL1A", "GB1", "GB2", "GB3", "GB4", "FIJ1", "FR1", "FR2", "L1", "L2", "L3", "GHPL", 
+                    "GR1", "GU1A", "GU1C", "HGKG", "UNG1", "IND1", "IN1L", "IRN1", "IR1", "ISR1", "IT1", "IT2", "JPL1", "JAP1", "RSK1", 
+                    "KG1L", "MYS1", "MEXA", "MARP", "MO1L", "MYA1", "NPL1", "NL1", "NCL1", "NNL1", "NO1", "PN1A", "PN1C", "PR1A", "PR1C", 
+                    "TDeA", "TDeC", "PFL1", "PL1", "PO1", "QSL", "RO1", "RU1", "SA1", "SC1", "SER1", "SIN1", "SFA1", "ES1", "ES2", "SE1", 
+                    "C1", "TAD1", "THA1", "TUN1", "TUSC", "TR1", "UKR1", "UAE1", "MLS1", "URU1", "UZ1", "VZ1L", "VIE1"]
+    
+    list_seasons = ["2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]
 
     def __init__(self):
         self.headers = {'User-Agent': self.USER_AGENT}
@@ -29,7 +35,7 @@ class TransfermarktScraper:
             for row in rows:
                 team_links = row.select('a')[0]['href']
                 print(team_links)
-                list_urls.append(team_links)
+                list_urls.append("https://www.transfermarkt.co.uk" + team_links)
 
         return list_urls
     
@@ -37,14 +43,20 @@ class TransfermarktScraper:
 
         list_player_url = set()
         for link in list_urls:
-            url = link
-            soup = self.scrape_webpage(url)
+            print(link)
+            r = requests.get(url=link, headers=headers)
+            soup = BeautifulSoup(r.content, 'html.parser')
             table = soup.find("table", class_='items')
-            rows = table.find_all('tr')
-            for row in rows:
-                cells = row.find_all('td', 'hauptlink')
-                if cells:
-                    player_link = cells[0].find('a')['href']
-                    list_player_url.add("https://www.transfermarkt.co.uk" + player_link)
+
+            if table is not None:
+                tbody = table.find("tbody")
+                rows = tbody.find_all("tr")
+                for row in rows:
+                    cells = row.find_all('td', 'hauptlink')
+                    if cells:
+                        player_link = cells[0].find('a')['href']
+                        list_player_url.add("https://www.transfermarkt.co.uk" + player_link)
+            else:
+                print("Table with class 'items' not found on the page.")
 
         list_player_url = list(list_player_url)
